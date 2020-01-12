c8ad2976d4d7647752215701c37efed065aa98bd
Ned <none@gmail.com>
2020-01-12 01:25:57 +0000

    now finished the learning module

--- myApp.js
+++ myApp.js
@@ -1,27 +1,35 @@
 
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
 
 
 // --> 11)  Mount the body-parser middleware  here
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function middleware(req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
 
 /** 1) Meet the node console. */
-console.log("Hello World");
-
-
+console.log("Hello World!");
 
 /** 2) A first working Express Server */
+
+
+
+/** 3) Serve an HTML file */
+
 app.get('/', function (req, res) {
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
   });
-
-/** 3) Serve an HTML file */
-
 
 /** 4) Serve static assets  */
 
@@ -29,24 +37,59 @@ app.use(express.static( __dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 
+let message = {"message": "Hello json"};
+
+app.get("/json", jsonHandler);
+
+function jsonHandler(req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    message.message = message.message.toUpperCase()
+  } 
+  return res.json(message);
+}
 
 /** 6) Use the .env file to configure the app */
- 
+ // Done
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
 
+
+
+
 /** 8) Chaining middleware. A Time server */
 
+const middleware = (req, res, next) => {
+  
+  req.time = "Sun Jan 12 2020 00:20:35 GMT+0000 (GMT)";
+  
+  next();
+};
+
+app.get("/now", middleware, (req, res) => {
+  res.json({
+    time: req.time
+  });
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+app.post("/name", (req, res) => {
+  var queryString = req.body.first + " " + req.body.last;
+  res.json({name: queryString});
+});
+
+// app.get("/name", (req, res) => {
+//   var queryString = req.query.first + " " + req.query.last;
+//   res.json({name: queryString});
+// });
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -62,3 +105,4 @@ app.use(express.static( __dirname + '/public'));
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
  module.exports = app;
+
